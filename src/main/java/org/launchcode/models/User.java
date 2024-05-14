3c05dc4b2be9a6cdf5857d36fd5701d6c92336d4
i-swa <ievaswanson@u.boisestate.edu>
2024-05-13 19:23:47 -0500

    user class added annotations for validation

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,8 +1,20 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class User {
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 5, max = 15, message = "Name must be between 5 and 15 characters")
     private String username;
+
+    @Email(message = "Invalid email. Try again.")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be between 6 or more characters")
     private String password;
 
     public User() {
